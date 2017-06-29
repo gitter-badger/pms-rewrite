55f858338239a7da14f6212188448933af79aa7b
Anuruddha Thennakoon <anuruddha.thennakoon@gmail.com>
2017-06-29 11:00:52 +0530

    modification in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './component/test/test.component';
 import { NavigationComponent } from './component/navigation/navigation.component';
 
 @NgModule({
