b1e853dcc4a0e3d1993e16f01e45c394ef2fc7d3
Shalitha Mihiranga <Shalitha Mihiranga>
2017-07-03 10:18:41 +0530

    add pansionable and payment info interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { PersonalInfoComponent } from './component/personal-info/personal-info.c
 import { ServiceInfoComponent } from './component/service-info/service-info.component';
 import { HeaderComponent } from './component/header/header.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { PensionableInfoComponent } from './component/pensionable-info/pensionable-info.component';
+import { PaymentInfoComponent } from './component/payment-info/payment-info.component';
 
 
 @NgModule({
@@ -27,7 +29,9 @@ import { DashboardComponent } from './component/dashboard/dashboard.component';
     PersonalInfoComponent,
     ServiceInfoComponent
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    PensionableInfoComponent,
+    PaymentInfoComponent
   ],
   imports: [
     BrowserModule
