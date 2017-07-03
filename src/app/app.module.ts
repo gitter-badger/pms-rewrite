eb5cf847ff19a25681d579e6b6a0e0f0ee953520
sasitha3 <sasitha.premadasa@my.sliit.lk>
2017-07-03 10:22:24 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './component/navigation/navigation.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { PendingAppComponent } from './component/pending/pending.component';
+import { ReceivedAppComponent } from './component/received/received.component';
+import { RejectedAppComponent } from './component/rejected/rejected.component';
+import { VerifiedAppComponent } from './component/verified/verified.component';
+import { PendingComponent } from './pending/pending.component';
+import { ReceivedComponent } from './received/received.component';
+import { RejectedComponent } from './rejected/rejected.component';
+import { VerifiedComponent } from './verified/verified.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    DashboardComponent,
+    PendingAppComponent,
+    ReceivedAppComponent,
+    RejectedAppComponent,
+    VerifiedAppComponent,
+    PendingComponent,
+    ReceivedComponent,
+    RejectedComponent,
+    VerifiedComponent
   ],
   imports: [
     BrowserModule
